cf13354c337ba5dc05a8b1cd8b35111879dd4f41
Michal-Sitarz <me@michalsitarz.com>
2018-11-13 12:37:51 +0000

    FINAL version

--- public_html/main.js
+++ public_html/main.js
@@ -1,5 +1,25 @@
+var valueToCountdown;
+var intervals;
+var tmr = document.getElementById("timer");
 
 function startCountdownTimer(){
-    alert("let's go!");
+    valueToCountdown = document.getElementById("inputTimer").value;
+    tmr.innerHTML = 'ready... set... go!';
+    intervals = window.setInterval(updateTimer, 1000);
 }
+
+function updateTimer(){
+     //s.style.color = s.style.color === 'red' ? 'blue' : 'red';
+    if(valueToCountdown>0){
+        tmr.innerHTML = valueToCountdown;
+        valueToCountdown--;
+    }else{
+        window.clearInterval(intervals);
+        tmr.innerHTML = 'KABOOM!!!';
+        document.getElementById("inputTimer").value = '';
+    }
+}
+
+
+
 
